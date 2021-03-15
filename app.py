7cb88a8f8cdbcb3688d108f599ec271c6bf4b1f5
Mark Grieves <techfilz@gmail.com>
2021-03-15 19:07:41 +0200

    Set up all imports

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+import os
+import yaml
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, flash
 from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# --- Configure DB
+db = yaml.safe_load(open('db.yaml'))
+app.config['MySQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
 Bootstrap(app)
+# --- Gets a random no for the flash msg
+app.config['SECRET_KEY'] = os.urandom(24)
 
 
+# --- APIs
 @app.route('/')
 def index():
     return render_template('index.html')
